7ae6bbbd5017d746854385e525b2ab539a0cf428
jncbhudiya <wrteam.jensibhudiya@gmail.com>
2025-07-11 10:28:24 +0530

    Blog Section is Done

--- src/app/-home/-blog/page.tsx
+++ src/app/-home/-blog/page.tsx
@@ -22,11 +22,11 @@ function Blog() {
     <div className="bg-[#FEF5E6] py-16 px-6 md:px-14 lg:px-[204px]">
       {/* Header */}
       <div className="flex flex-col md:flex-row w-full max-w-[1320px] mx-auto justify-between items-start md:items-end mb-10 md:mb-12 gap-6">
-        <div className="w-full md:max-w-[645px] space-y-2">
-          <h2 className="text-2xl md:text-[40px] font-bold text-[#2D2C2F] font-ubuntu leading-[36px] md:leading-[48px] tracking-normal">
+        <div className="w-full md:max-w-[645px]  md:max-h-[80px] space-y-2">
+          <h2 className="text-5xl md:text-[40px] font-bold text-[#2D2C2F] font-[ubuntu] leading-[36px] md:leading-[48px] tracking-normal">
             The Travel Blog
           </h2>
-          <p className="text-[#464549] text-sm md:text-[16px] font-medium font-ubuntu leading-[24px]">
+          <p className="text-[#464549] text-sm md:text-[16px] font-medium font-[ubuntu]    leading-[24px]">
             Get inspired, informed, and entertained with our travel stories,
             tips, and guides.
           </p>
@@ -57,7 +57,7 @@ function Blog() {
         {blogPosts.map((post) => (
           <div
             key={post.id}
-            className="shrink-0 w-[85%] sm:w-[360px] md:w-[420px] h-auto p-4 bg-white rounded-[30px] shadow-sm snap-start"
+            className="shrink-0 w-[85%] sm:w-[360px] md:w-[420px] md:h-[450px] p-4 bg-white rounded-[30px] shadow-sm snap-start"
           >
             <img
               className="w-full h-[180px] sm:h-[200px] object-cover rounded-xl"
@@ -65,15 +65,15 @@ function Blog() {
               alt={post.title}
             />
             <div className="w-full flex flex-col gap-[12px] pt-[16px]">
-              <h1 className="text-[18px] sm:text-[20px] font-bold leading-[24px] text-[#2D2C2F] font-ubuntu line-clamp-2">
+              <h1 className="text-[18px] sm:text-[20px] font-bold leading-[24px] text-[#2D2C2F] font-[ubuntu] line-clamp-2">
                 {post.title}
               </h1>
-              <p className="text-[14px] sm:text-[16px] leading-[24px] text-[#464549] font-ubuntu line-clamp-2">
+              <p className="text-[14px] sm:text-[16px] leading-[24px] text-[#464549] font-[ubuntu] line-clamp-2">
                 {post.description}
               </p>
             </div>
 
-            <button className="mt-6 text-[#EC9105] text-[16px] sm:text-[20px] leading-[30px] font-normal font-ubuntu flex items-center gap-1">
+            <button className="mt-14 text-[#EC9105] text-[16px] sm:text-[20px] leading-[30px] font-normal font-[ubuntu] flex items-center gap-1">
               Read More
             </button>
           </div>
@@ -82,7 +82,7 @@ function Blog() {
 
       {/* View All Button */}
       <div className="flex justify-center mt-10">
-        <button className="px-6 sm:px-8 py-3 bg-[#EC9105] text-white font-semibold rounded-full shadow-lg transition duration-300 ease-in-out flex items-center space-x-2">
+        <button className="px-6 sm:px-8 py-3 bg-[#EC9105] text-white font-semibold  font-[ubuntu] rounded-full shadow-lg transition duration-300 ease-in-out flex items-center space-x-2">
           <span>View All Blogs</span> <RightArrow />
         </button>
       </div>
